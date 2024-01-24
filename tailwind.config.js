82b233567f86d1ead9c56fb1d955f3de054e5c7d
Henry Reith <henryreith@users.noreply.github.com>
2024-01-24 18:36:48 +1100

    Update tailwind config with new font sizes and sub-menu update

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,14 +16,36 @@ module.exports = {
       xlg: '1200px',
       pageMaxWidth: '1300px',
     },
+    fontSize: {
+      sm: '1.2rem',
+      base: '1.5rem',
+      xl: 'calc(1.7rem * var(--font-heading-scale))',
+      '2xl': 'calc(2rem * var(--font-heading-scale))',
+      '3xl': 'calc(3rem * var(--font-heading-scale))',
+      '4xl': 'calc(4rem * var(--font-heading-scale))',
+      '5xl': 'calc(5rem * var(--font-heading-scale))',
+    },
     extend: {
       fontFamily: {
         heading: 'var(--font-heading-family)',
+      },
+      fontSize: {
+        // Custom Heading Sizes for Mobile
+        hxl: 'calc(5rem * var(--font-heading-scale))', // Custom class for extra large heading
+        h0: 'calc(4rem * var(--font-heading-scale))', // Custom class for large heading
+        h1: 'calc(3rem * var(--font-heading-scale))', // h1 size
+        h2: 'calc(2rem * var(--font-heading-scale))', // h2 size
+        h3: 'calc(1.7rem * var(--font-heading-scale))', // h3 size
+        h4: 'calc(1.5rem * var(--font-heading-scale))', // h4 size
+        h5: 'calc(1.2rem * var(--font-heading-scale))', // h5 size
+        h6: 'calc(1rem * var(--font-heading-scale))',  // h6 size
       },
     },
     color: {
       black: '#000000',
     },
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 };
