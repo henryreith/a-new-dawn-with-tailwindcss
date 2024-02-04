abfad33d2bb67bf32b5c34d330702656729ea210
Henry Reith <henryreith@users.noreply.github.com>
2024-02-04 14:16:14 +1100

    Change to run highlighted products when the dom has loaded

--- src/js/highlighted_products.js
+++ src/js/highlighted_products.js
@@ -1,9 +1,11 @@
-const swiper = new Swiper('.highlighted-products__swiper', {  
-  navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  lazy: true,
-  loop: true,
+// Wait for DOM content to load before initializing Swipers again
+document.addEventListener('DOMContentLoaded', (event) => {
+  const swiper = new Swiper('.highlighted-products__swiper', {  
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    lazy: true,
+    loop: true,
+  });
 });
- 
