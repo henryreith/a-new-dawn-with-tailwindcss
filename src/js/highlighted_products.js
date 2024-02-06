8e491d73bb9eb2bafdda279a7c2a1b4f1e2ffdb4
Henry Reith <henryreith@users.noreply.github.com>
2024-02-06 15:58:03 +1100

    Fix screen size change issues with section to it goes from 1 col grif to 3 col on desktop, and refact js to load swiper correctly.

--- src/js/highlighted_products.js
+++ src/js/highlighted_products.js
@@ -1,11 +1,26 @@
-// Wait for DOM content to load before initializing Swipers again
-document.addEventListener('DOMContentLoaded', (event) => {
-  const swiper = new Swiper('.highlighted-products__swiper', {  
+import Swiper from 'swiper';
+
+import { Navigation } from 'swiper/modules';
+
+// Configuration for the highlighted products Swiper slider
+const HighlightedProductsSwiperConfig = {
+  modules: [Navigation],
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
     lazy: true,
     loop: true,
-  });
+};
+
+// Initializes the Swiper slider for highlighted products
+const initHighlightedProductsSwiper = () => {
+  new Swiper(".highlighted-products__swiper", HighlightedProductsSwiperConfig);
+};
+
+let highlightedProductsSwiper;
+
+// Wait for the DOM to fully load before initializing the Swiper slider
+document.addEventListener('DOMContentLoaded', () => {
+  highlightedProductsSwiper = initHighlightedProductsSwiper();
 });
